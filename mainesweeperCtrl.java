ce5a6dff48cbffda23c5a954c5a2d046d242a40f
KHSCS003 <KHSCS003@KATYISD.ORG>
2018-10-02 09:41:11 -0500

    woah no way it's an initial commit that's avgvbsolutely FLABERGASTING

---
+++ mainesweeperCtrl.java
@@ -0,0 +1,12 @@
+package MAINesweeper;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class mainesweeperCtrl extends Application {
+    public static void main(String[]args){
+        launch(args);
+    }
+    public void start(Stage stage){
+    }
+}
